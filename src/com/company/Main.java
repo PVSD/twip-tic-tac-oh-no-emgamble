58ebc23d9a13a2df34b4c70742dd45a1164fe7f4
emgamble <eg913@pvsd.org>
2019-01-01 14:16:44 -0500

    still a work in progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,6 +19,14 @@ public class Main {
             }
             else
                 System.out.println("Sorry that slot is already full");
+            System.out.println(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ");
+            System.out.println("-----------------------------------------------------------------");
+            System.out.println(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ");
+            System.out.println("-----------------------------------------------------------------");
+            System.out.println(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " ");
+            System.out.println("-----------------------------------------------------------------");
+
+
 
             System.out.println("Player O please input your first move as a coordinate (x y), remember the board starts at 0.");
             int a = playerO.nextInt();
@@ -28,22 +36,48 @@ public class Main {
             }
             else
                 System.out.println("Sorry that slot is already full");
+            System.out.println(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ");
+            System.out.println("-----------------------------------------------------------------");
+            System.out.println(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ");
+            System.out.println("-----------------------------------------------------------------");
+            System.out.println(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " ");
+            System.out.println("-----------------------------------------------------------------");
 
-            String line = null;
             int testWinner = 8;
-            switch (testWinner){
-                case 1: line = board[0][0] + board[0][1] + board[0][2];
-                    break;
-                case 2: line = board[1][0] + board[1][1] + board[1][2];
-                    break;
-                case 3: line = board[2][0] + board[2][1] + board[2][2];
-                    break;
-                case 4: line = board[0][1] + board[1][1] + board[2][1];
-                    break;
-                case 5: line = board[0][2] + board[1][2] + board[2][2];
-                    break;
-                case 6: line = board[0][0] + board[1][0] + board[2][0];
-                    break;
+            String line = null;
+            switch (testWinner) {
+                    case 1:
+                        line = board[0][0] + board[0][1] + board[0][2];
+                        break;
+                    case 2:
+                        line = board[1][0] + board[1][1] + board[1][2];
+                        break;
+                    case 3:
+                        line = board[2][0] + board[2][1] + board[2][2];
+                        break;
+                    case 4:
+                        line = board[0][1] + board[1][1] + board[2][1];
+                        break;
+                    case 5:
+                        line = board[0][2] + board[1][2] + board[2][2];
+                        break;
+                    case 6:
+                        line = board[0][0] + board[1][0] + board[2][0];
+                        break;
+                    case 7:
+                        line = board[0][0] + board[1][1] + board[2][2];
+                        break;
+                    case 8:
+                        line = board[2][0] + board[1][1] + board[0][2];
+                        break;
+                }
+            if(line.equals("XXX")) {
+                System.out.println("Player X is the winner");
+                break;
+            }
+            else if(line.equals("OOO")) {
+                System.out.println("Player O is the winner");
+                break;
             }
 
         }
